cf1472fbfd3356bd1fc610bced317988f81c4c33
Haziq Khairi <ikhairulhaziq11@gmail.com>
2024-05-12 20:10:13 +0800

    feat: add new asset and image

--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -1,3 +1,8 @@
 abstract class Assets {
+  // Icons
   static const String electricIcon = 'assets/icons/electric-icon.svg';
+  static const String infoIcon = 'assets/icons/info-icon.svg';
+
+  // Images
+  static const String myPicture = 'assets/images/my-picture.jpg';
 }
